62b6104187b9a403f1664a22417867afcf0360d1
Sergey-air-779 <michinssergeevich@gmail.com>
2024-10-27 22:16:31 +0500

    new commit

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 function button_color_1() {
     document.getElementById('text').style.color = 'red';
 }
+
+function button_color_2() {
+    document.getElementById('text').style.color = 'blue';
+}
+
+function button_color_3() {
+    document.getElementById('text').style.color = 'yellow';
+}
